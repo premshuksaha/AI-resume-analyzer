1cc4a3ec4ef1fa1cd2ae537eb9d7b0da3282e4fd
premshuksaha <premshuk2020@gmail.com>
2026-02-16 20:25:40 +0530

    Fetched resume upload history on the homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,22 +1,22 @@
 import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
 import {useEffect, useState} from "react";
-//import {usePuterStore} from "~/lib/puter";
+import {usePuterStore} from "~/lib/puter";
 
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
-    //const { fs } = usePuterStore();
+    const { fs } = usePuterStore();
     const [resumeUrl, setResumeUrl] = useState('');
 
-    // useEffect(() => {
-    //     const loadResume = async () => {
-    //         const blob = await fs.read(imagePath);
-    //         if(!blob) return;
-    //         let url = URL.createObjectURL(blob);
-    //         setResumeUrl(url);
-    //     }
+    useEffect(() => {
+        const loadResume = async () => {
+            const blob = await fs.read(imagePath);
+            if(!blob) return;
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+        }
 
-    //     loadResume();
-    // }, [imagePath]);
+        loadResume();
+    }, [imagePath]);
 
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
